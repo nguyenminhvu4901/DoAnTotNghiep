2f5473e504aa580da8b8c913956097f53ac099a9
vunm <vunm@deha-soft.com>
2023-12-20 15:08:08 +0700

    chat_base

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,14 +12,14 @@ const mix = require('laravel-mix');
  */
 
 mix.setPublicPath('public')
-    .setResourceRoot('../') // Turns assets paths in css relative to css file
+    .setResourceRoot('../')
+    .react() // Turns assets paths in css relative to css file
     .vue()
     .sass('resources/sass/frontend/app.scss', 'css/frontend.css')
     .sass('resources/sass/backend/app.scss', 'css/backend.css')
     .js('resources/js/frontend/app.js', 'js/frontend.js')
     .js('resources/js/backend/app.js', 'js/backend.js')
     .js('resources/js/app.js', 'js/app.js')
-    .react()
     .copyDirectory('resources/js/frontend/pages', 'public/js/pages')
     .copyDirectory('resources/js/frontend/assets', 'public/js/assets')
     .copyDirectory('resources/js/frontend/layouts', 'public/js/layouts')
