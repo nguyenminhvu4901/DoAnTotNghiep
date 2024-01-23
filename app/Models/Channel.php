0dc77d38b170bf72657af67a5fb038217737c3ff
vunm <vunm@deha-soft.com>
2024-01-23 20:55:04 +0700

    build base logic chat

--- app/Models/Channel.php
+++ app/Models/Channel.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use App\Domains\Auth\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Channel extends Model
 {
     use HasFactory;
+
+    protected $table = 'channels';
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'channel_user', 'channel_id', 'user_id');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class, 'channel_id', 'id');
+    }
 }
